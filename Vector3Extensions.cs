0b1a224697bbd6a78120c595a3a2bb13311a3aa1
Savva Madar <37639781+savvamadar@users.noreply.github.com>
2023-06-06 12:00:13 -0400

    Update Vector3Extensions.cs

--- Vector3Extensions.cs
+++ Vector3Extensions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vector3Extensions
+public static class Vector3Extensions
 {
     public static float InverseLerp(Vector3 a, Vector3 b, Vector3 value)
     {
@@ -10,9 +10,13 @@ public class Vector3Extensions
         Vector3 AV = value - a;
         return Mathf.Clamp01(Vector3.Dot(AV, AB) / Vector3.Dot(AB, AB));
     }
-    
+
     public static Vector3 RelativeLocalPosition(Transform parent, Transform child)
     {
+        if(parent == child)
+        {
+            return parent.localPosition;
+        }
         return parent.InverseTransformPoint(child.position);
     }
 }
